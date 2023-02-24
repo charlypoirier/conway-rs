414cdefedf00a2b692d9f2cefd73390fd946d215
charlypoirier <pro.poiriercharly@gmail.com>
2023-02-24 23:17:47 +0100

    Filling the terminal with characters using a loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let (w, h) = term_size::dimensions().unwrap();
+    let delay = std::time::Duration::from_millis(250);
+
+    let mut character = '█';
+    loop {
+        for _ in 0..h {
+            for _ in 0..w {
+                print!("{}", character);
+            }
+            println!();
+        }
+        character = if character == '█' { ' ' } else { '█' };
+
+        std::thread::sleep(delay);
+        clear_console();
+    }
+}
+
+fn clear_console() {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 }
