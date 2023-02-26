5f8bd5f34ca921006f97fe35d494227267429bbf
charlypoirier <pro.poiriercharly@gmail.com>
2023-02-26 14:38:52 +0100

    Create and display a grid of random cells

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-fn main() {
-    let (w, h) = term_size::dimensions().unwrap();
-    let delay = std::time::Duration::from_millis(250);
-
-    let mut character = '█';
-    loop {
-        for _ in 0..h {
-            for _ in 0..w {
-                print!("{}", character);
-            }
-            println!();
-        }
-        character = if character == '█' { ' ' } else { '█' };
-
-        std::thread::sleep(delay);
-        clear_console();
-    }
-}
+use rand::{thread_rng, Rng};
 
 fn clear_console() {
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+}
+
+fn init_cells(width: usize, height: usize) -> Vec<Vec<bool>> {
+    let mut cells = vec![vec![true; width]; height];
+    let mut rng = thread_rng();
+    for i in 0..height {
+        for j in 0..width {
+            cells[i][j] = rng.gen_bool(0.3);
+        }
+    }
+    return cells;
+}
+
+fn main() {
+    let (width, height) = term_size::dimensions().unwrap();
+    let cells = init_cells(width, height);
+
+    clear_console();
+
+    for i in 0..height {
+        for j in 0..width {
+            match cells[i][j] {
+                true => print!("█"),
+                false => print!(" "),
+            }
+        }
+        println!();
+    }
 }
